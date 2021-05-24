d6d0a59521c1db5b377f8eac5570163fb1580cf0
DorukBilgi <84389609+DorukBilgi@users.noreply.github.com>
2021-05-24 10:26:45 +0300

    Update BRAM_wrapper.vhd

--- BRAM/BRAM_wrapper.vhd
+++ BRAM/BRAM_wrapper.vhd
@@ -1,12 +1,6 @@
---Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
-----------------------------------------------------------------------------------
---Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
---Date        : Sun May 23 02:26:01 2021
---Host        : afb01 running 64-bit major release  (build 9200)
---Command     : generate_target BRAM_wrapper.bd
---Design      : BRAM_wrapper
---Purpose     : IP block netlist
-----------------------------------------------------------------------------------
+--bu dosya block designdan sonra program tarafından otomatik olarak oluşturuldu.
+--Ben yazmadım.
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library UNISIM;
