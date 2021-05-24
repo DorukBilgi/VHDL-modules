aa2af2037da88a1a72a24c92a531fda94c0f4169
DorukBilgi <84389609+DorukBilgi@users.noreply.github.com>
2021-05-24 15:59:25 +0300

    Update BRAM_tb.vhd

--- BRAM/BRAM_tb.vhd
+++ BRAM/BRAM_tb.vhd
@@ -1,80 +1,106 @@
--- Program tarafından oluşturulmuş olan BRAM_wrapper dosyası için testbench dosyası.
--- Bu dosya ile block ram karakteristiği gözlemlenmek istendi.
-
-library IEEE;
-use IEEE.Std_logic_1164.all;
-use IEEE.Numeric_Std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+-------------------------------------------------------------------------------
 
 entity BRAM_wrapper_tb is
-end;
 
-architecture bench of BRAM_wrapper_tb is
+end entity BRAM_wrapper_tb;
 
-  component BRAM_wrapper
-    port (
-      BRAM_PORTA_0_addr : in STD_LOGIC_VECTOR ( 31 downto 0 );
-      BRAM_PORTA_0_clk  : in STD_LOGIC;
-      BRAM_PORTA_0_din  : in STD_LOGIC_VECTOR ( 31 downto 0 );
-      BRAM_PORTA_0_dout : out STD_LOGIC_VECTOR ( 31 downto 0 );
-      BRAM_PORTA_0_en   : in STD_LOGIC;
-      BRAM_PORTA_0_rst  : in STD_LOGIC;
-      BRAM_PORTA_0_we   : in STD_LOGIC_VECTOR ( 3 downto 0 )
-    );
-  end component;
+-------------------------------------------------------------------------------
 
-  signal BRAM_PORTA_0_addr  : STD_LOGIC_VECTOR ( 31 downto 0 );
-  signal BRAM_PORTA_0_clk   : STD_LOGIC;
-  signal BRAM_PORTA_0_din   : STD_LOGIC_VECTOR ( 31 downto 0 );
-  signal BRAM_PORTA_0_dout  : STD_LOGIC_VECTOR ( 31 downto 0 );
-  signal BRAM_PORTA_0_en    : STD_LOGIC;
-  signal BRAM_PORTA_0_rst   : STD_LOGIC;
-  signal BRAM_PORTA_0_we    : STD_LOGIC_VECTOR ( 3 downto 0 ) ;
+architecture rtl of BRAM_wrapper_tb is
 
-begin
+  -- component ports
+  signal BRAM_PORTA_0_addr : STD_LOGIC_VECTOR (31 downto 0);
+  signal BRAM_PORTA_0_din  : STD_LOGIC_VECTOR (31 downto 0);
+  signal BRAM_PORTA_0_dout : STD_LOGIC_VECTOR (31 downto 0);
+  signal BRAM_PORTA_0_en   : STD_LOGIC;
+  signal BRAM_PORTA_0_rst  : STD_LOGIC;
+  signal BRAM_PORTA_0_we   : STD_LOGIC_VECTOR (3 downto 0);
 
-  uut: BRAM_wrapper port map ( 
+  -- clock
+  signal clk : std_logic := '1';
+
+  signal addr_temp : std_logic_vector(31 downto 0) := (others => '0');
+  signal din_temp : std_logic_vector(31 downto 0) := (others => '0');
+  signal sayac_temp : std_logic_vector(7 downto 0) := (others => '0');
+  
+begin  -- architecture rtl
+
+  -- component instantiation
+  DUT: entity work.BRAM_wrapper
+    port map (
       BRAM_PORTA_0_addr => BRAM_PORTA_0_addr,
-      BRAM_PORTA_0_clk  => BRAM_PORTA_0_clk,
+      BRAM_PORTA_0_clk  => clk,
       BRAM_PORTA_0_din  => BRAM_PORTA_0_din,
       BRAM_PORTA_0_dout => BRAM_PORTA_0_dout,
       BRAM_PORTA_0_en   => BRAM_PORTA_0_en,
       BRAM_PORTA_0_rst  => BRAM_PORTA_0_rst,
-      BRAM_PORTA_0_we   => BRAM_PORTA_0_we );
+      BRAM_PORTA_0_we   => BRAM_PORTA_0_we);
 
-    clock_process: process
-    begin
-         BRAM_PORTA_0_clk <= '1';
-         wait for 1 ns;
-         BRAM_PORTA_0_clk <= '0';
-         wait for 1 ns;
-    end process;
-    
-    stimulus_process: process
-    begin 
-         BRAM_PORTA_0_rst    <= '0';
-         BRAM_PORTA_0_addr   <= "00000000000000000000000000000000";
-         BRAM_PORTA_0_din    <= "00000000000000000000000000000000";
-         wait for 12 ns;
-            
-        for i in 0 to 255 loop
-            BRAM_PORTA_0_en <= '1';  --Enable RAM always.
-            BRAM_PORTA_0_we <= "1111";
-            wait for 2 ns;
-            BRAM_PORTA_0_addr <= std_logic_vector(unsigned(BRAM_PORTA_0_addr) + 1);
-            BRAM_PORTA_0_din  <= std_logic_vector(unsigned(BRAM_PORTA_0_din) + 1);
-        end loop;  
-        
-        BRAM_PORTA_0_addr  <= "00000000000000000000000000000000";  --reset the address value for reading from memory location "0"
-        BRAM_PORTA_0_din   <= "00000000000000000000000000000000";
+
   
-        for i in 0 to 255 loop
-            BRAM_PORTA_0_en <= '1';  --Enable RAM always.
-            BRAM_PORTA_0_we <= "0000";
-            wait for 2 ns;
-            BRAM_PORTA_0_addr <= std_logic_vector(unsigned(BRAM_PORTA_0_addr) + 1);
-        end loop;
-       
-          wait;
-    end process;
-    
-end;
+  -- clock generation
+  clk <= not clk after 10 ns;
+
+  -- waveform generation
+  WaveGen_Proc: process
+  begin
+                
+    wait until clk = '1';
+  end process WaveGen_Proc;
+
+
+  writePro: process (clk) is
+  begin  -- process writePro
+    if clk'event and clk = '1' then  -- rising clock edge
+            
+       BRAM_PORTA_0_en     <= '1';  --Enable RAM always. 
+       BRAM_PORTA_0_rst    <= '0'; -- reset yok
+       BRAM_PORTA_0_addr   <= addr_temp;  
+       BRAM_PORTA_0_din    <= din_temp;
+
+       if(sayac_temp <= "00001110")then -- yazma islemi devam
+           
+        BRAM_PORTA_0_we     <= "1111";
+        sayac_temp          <= std_logic_vector(unsigned(sayac_temp) + 1);
+        addr_temp           <= std_logic_vector(unsigned(addr_temp) + 1);
+        din_temp            <= std_logic_vector(unsigned(din_temp) + 1);
+             
+       end if;
+           
+       if(sayac_temp = "00001111")then
+           
+        BRAM_PORTA_0_we     <= "0000"; -- write off mode 
+        din_temp            <= "00000000000000000000000000000000"; -- gelen data default
+        addr_temp           <= "00000000000000000000000000000100"; --reset the address value for reading from memory location "4"
+        -- neden bilmiyorum 120 ns sonra verdiğim dataları çıkışta gözlemleyebiliyorum. başlangıçta 120 ns boyunca dataout 0
+        sayac_temp          <= std_logic_vector(unsigned(sayac_temp) + 1);
+           
+       end if;
+           
+       if(sayac_temp > "00001111")then -- yazma islemi bitti. okuma yapılacak.
+            
+        BRAM_PORTA_0_we     <= "0000"; -- write off mode 
+        din_temp            <= "00000000000000000000000000000000"; -- gelen data default
+        sayac_temp          <= std_logic_vector(unsigned(sayac_temp) + 1);
+        addr_temp           <= std_logic_vector(unsigned(addr_temp) + 1);
+            
+       end if;
+
+    end if;
+  end process writePro;
+
+
+
+end architecture rtl;
+
+-------------------------------------------------------------------------------
+
+configuration BRAM_wrapper_tb_rtl_cfg of BRAM_wrapper_tb is
+  for rtl
+  end for;
+end BRAM_wrapper_tb_rtl_cfg;
+
+-------------------------------------------------------------------------------
